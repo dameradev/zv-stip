dae2eb61638496af05b55b1d24638c5984e42bd9
Your Name <radevdamjan@gmail.com>
2024-07-05 12:06:29 +0200

    Change meta tags

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -3,8 +3,9 @@ module.exports = {
   // NOTE: `process.env.URL` is provided by Netlify, and may need
   // adjusted pending your host
   url: process.env.URL || "http://localhost:8080",
-  siteName: "SpeedStack",
-  siteDescription: "",
+  siteName: "Здружение на возачи Штип",
+  siteDescription:
+    "Нашите услуги се дизајнирани да ги задоволат сите ваши потреби за регистрирање, осигурување и проверка на техничката исправност на вашето возило.",
   authorName: "",
   twitterUsername: "", // no `@`
   primaryNavigation: [
